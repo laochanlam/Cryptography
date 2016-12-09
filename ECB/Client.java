a2ea9385155e08b2a5ea8e59ae3c698b410166aa
LaM <laochanlam@gmail.com>
2016-12-10 00:34:22 +0800

    bug fixed but CTR Still

--- ECB/Client.java
+++ ECB/Client.java
@@ -1,54 +1,155 @@
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.io.OutputStream;
-import java.io.InputStream;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import javax.crypto.*;
 import java.security.*;
 import sun.misc.*;
 import javax.crypto.spec.*;
+import java.io.*;
+import java.util.Scanner;
 
 public class Client {
 
 
-  public static int port = 3002;
-  public static void main(String args[]) throws Exception {
+public static int port = 3002;
+public static void main(String args[]) throws Exception {
 
-    String plainText = "Hello ,This is ECB !";
-    String key = "12345678";
-    byte [] iv = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-    IvParameterSpec ivspec = new IvParameterSpec(iv);
-	//Key , PlainText & IV
 
-    SecureRandom random = new SecureRandom(); 
-	//random num generation
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ECB:1 CBC:2 CFB:3 OFB:4 CTR:5");
 
-    DESKeySpec keyspec = new DESKeySpec(key.getBytes());
-    SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
-    SecretKey secretkey = keyFactory.generateSecret(keyspec);
-	//key generation
+        Socket client = new Socket("127.0.0.1", port);
+        DataOutputStream out = new DataOutputStream(client.getOutputStream());
+        int option = scanner.nextInt();
 
-    Cipher cipher = Cipher.getInstance("DES/ECB/PKCS5Padding");
-    cipher.init(Cipher.ENCRYPT_MODE, secretkey , random);
-    byte[] cipherData = cipher.doFinal(plainText.getBytes("UTF-8"));
-	//Encrypt
+        Scanner anotherscanner = new Scanner(System.in);
+        String plainText = anotherscanner.nextLine();
 
-    Socket client = new Socket("127.0.0.1", port);
-    	//connect server
+        String key = "12345678";
+        byte [] iv = {1,1,1,1,0,0,0,0};
+        IvParameterSpec ivspec = new IvParameterSpec(iv);
+        //Key , PlainText & IV
 
-    System.out.println("<<<<ECB>>>>");
-    System.out.println("PlainText :" + plainText);
-    System.out.println("CipherText:" + cipherData);
-	//print P & C & Method on screen 
+        SecureRandom random = new SecureRandom();
+        //random num generation
 
-    OutputStream sent = client.getOutputStream();
-    sent.write(cipherData);
-	//send data
+        DESKeySpec keyspec = new DESKeySpec(key.getBytes());
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+        SecretKey secretkey = keyFactory.generateSecret(keyspec);
+        //key generation
 
-    sent.close();
-    client.close();
 
-  }
+        out.writeInt(option);
+///////////////////////////option////////////////////////////////////
+
+        switch(option) {
+
+
+        case 1: //ECB
+
+
+
+                Cipher cipherECB = Cipher.getInstance("DES/ECB/PKCS5Padding");
+                cipherECB.init(Cipher.ENCRYPT_MODE, secretkey, random);
+                byte[] cipherDataECB = cipherECB.doFinal(plainText.getBytes("UTF-8"));
+                //Encrypt
+
+                System.out.println("<<<<ECB>>>>");
+                System.out.println("PlainText : " + plainText);
+
+                //print P & C & Method on screen
+
+                OutputStream sentECB = client.getOutputStream();
+                sentECB.write(cipherDataECB);
+                //send data
+
+                sentECB.close();
+                client.close();
+                break;
+
+        case 2: //CFB
+
+                Cipher cipherCFB = Cipher.getInstance("DES/CFB/PKCS5Padding");
+                cipherCFB.init(Cipher.ENCRYPT_MODE, secretkey, ivspec);
+                byte[] cipherDataCFB = cipherCFB.doFinal(plainText.getBytes("UTF-8"));
+                //Encrypt
+
+                System.out.println("<<<<CFB>>>>");
+                System.out.println("PlainText :" + plainText);
+
+                //print P & C & Method on screen
+
+                OutputStream sentCFB = client.getOutputStream();
+                sentCFB.write(cipherDataCFB);
+                //send data
+
+                sentCFB.close();
+                client.close();
+                break;
+
+        case 3: //CBC
+
+                Cipher cipherCBC = Cipher.getInstance("DES/CBC/PKCS5Padding");
+                cipherCBC.init(Cipher.ENCRYPT_MODE, secretkey, ivspec);
+                byte[] cipherDataCBC = cipherCBC.doFinal(plainText.getBytes("UTF-8"));
+                //Encrypt
+
+                System.out.println("<<<<CBC>>>>");
+                System.out.println("PlainText :" + plainText);
+
+                //print P & C & Method on screen
+
+                OutputStream sentCBC = client.getOutputStream();
+                sentCBC.write(cipherDataCBC);
+                //send data
+
+                sentCBC.close();
+                client.close();
+                break;
+
+        case 4:                 //OFB
+
+                Cipher cipherOFB = Cipher.getInstance("DES/OFB/PKCS5Padding");
+                cipherOFB.init(Cipher.ENCRYPT_MODE, secretkey, ivspec);
+                byte[] cipherDataOFB = cipherOFB.doFinal(plainText.getBytes("UTF-8"));
+                //Encrypt
+
+                System.out.println("<<<<OFB>>>>");
+                System.out.println("PlainText :" + plainText);
+
+                //print P & C & Method on screen
+
+                OutputStream sentOFB = client.getOutputStream();
+                sentOFB.write(cipherDataOFB);
+                //send data
+
+                sentOFB.close();
+                client.close();
+                break;
+
+
+        case 5:         //CTR
+
+                Cipher cipherCTR = Cipher.getInstance("DES/CTR/NoPadding");
+                cipherCTR.init(Cipher.ENCRYPT_MODE, secretkey, ivspec);
+                byte[] cipherDataCTR = cipherCTR.doFinal(plainText.getBytes("UTF-8"));
+                //Encrypt
+
+                System.out.println("<<<<CTR>>>>");
+                System.out.println("PlainText :" + plainText);
+
+                //print P & C & Method on screen
+
+                OutputStream sentCTR = client.getOutputStream();
+                sentCTR.write(cipherDataCTR);
+                //send data
+
+                sentCTR.close();
+                client.close();
+                break;
+
+
+        }
+
+
 }
-
+}
--- ECB/Server.java
+++ ECB/Server.java
@@ -7,55 +7,194 @@ import javax.crypto.*;
 import java.security.*;
 import sun.misc.*;
 import javax.crypto.spec.*;
+import java.awt.geom.Dimension2D;
 
 public class Server {
-  public static int port = 3002;
+public static int port = 3002;
 
-  public static void main(String args[]) throws Exception {
-    ServerSocket ss = new ServerSocket(port);
-    byte[] iv = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
-    IvParameterSpec ivspec = new IvParameterSpec(iv);
+public static void main(String args[]) throws Exception {
+        ServerSocket ss = new ServerSocket(port);
 
-    while (true) { //if connected
-      Socket server = ss.accept();
 
-      System.out.println("Someone Connected");
+        while (true) { //if connected
+                Socket server = ss.accept();
 
-/////////////////////////ECB/////////////////////////
+                System.out.println("Someone Connected");
+                byte[] iv = {1,1,1,1,0,0,0,0};
+                IvParameterSpec ivspec = new IvParameterSpec(iv);
 
-      InputStream input = server.getInputStream();
-      BufferedReader reader = new BufferedReader(
-      	new InputStreamReader(input)
-      );
-      int value = 0;
-      byte[] buffer = new byte[24];
-      while((value=input.read(buffer)) != -1);
-	//Receive & format data	
 
-      String key = "12345678";
-      DESKeySpec keyspec = new DESKeySpec(key.getBytes("UTF-8"));
-      SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
-      SecretKey secretkey = keyFactory.generateSecret(keyspec);
-	//key generation
+                String key = "12345678";
+                DESKeySpec keyspec = new DESKeySpec(key.getBytes("UTF-8"));
+                SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+                SecretKey secretkey = keyFactory.generateSecret(keyspec);
+                //key generation
 
-   
-      Cipher cipher = Cipher.getInstance("DES/ECB/NoPadding");
-      cipher.init(Cipher.DECRYPT_MODE , secretkey);
-      byte[] plainData = cipher.doFinal(buffer);
-	//Decrypt
+                DataInputStream in = new DataInputStream(server.getInputStream());
+                int option = in.readInt();
 
-      System.out.println(new String(plainData));
-	//print out P 
+                System.out.println(option);
+                int length = 0;
+                int i = 0;
 
-      input.close();
-
-/////////////////////////ECB//////////////////////////
+                switch(option) {
+                case 1:
+// /////////////////////////ECB/////////////////////////
+                        InputStream inputECB = server.getInputStream();
 
 
 
+                        Cipher cipherECB = Cipher.getInstance("DES/ECB/NoPadding");
+                        cipherECB.init(Cipher.DECRYPT_MODE, secretkey);
 
-      server.close();
 
-    }
-  }
+                        int valueECB = 0;
+                        byte[] bufferECB = new byte[8];
+                        while((valueECB=inputECB.read(bufferECB)) != -1) {
+                                byte[] plainTextECB = cipherECB.doFinal(bufferECB);
+                                System.out.print(new String(plainTextECB));
+                        }
+                        inputECB.close();
+                        server.close();
+                        // /////////////////////////ECB//////////////////////////
+                        break;
+
+
+                case 2:         //CFB
+                                /////////////////////////CFB/////////////////////////
+                                /////////////////////////CFB/////////////////////////
+                        InputStream inputCFB = server.getInputStream();
+
+
+
+                        Cipher cipherCFB = Cipher.getInstance("DES/CFB/NoPadding");
+                        cipherCFB.init(Cipher.DECRYPT_MODE, secretkey, ivspec);
+
+
+                        int valueCFB = 0;
+                        byte[] bufferCFB = new byte[8];
+                        byte[] bufferCFB2 = new byte[256];
+                        while((valueCFB=inputCFB.read(bufferCFB)) != -1) {
+                                for (i=0; i<8; i++)
+                                        bufferCFB2[i+length] = bufferCFB[i];
+                                length = length+8;
+                        }
+                        byte[] realdoCFB = new byte[length];
+                        for (i=0; i<length; i++)
+                                realdoCFB[i] = bufferCFB2[i];
+
+
+
+                        byte[] plainTextCFB = cipherCFB.doFinal(realdoCFB);
+                        System.out.print(new String(plainTextCFB));
+
+
+                        inputCFB.close();
+                        server.close();
+
+                        break;
+
+                case 3:                 //CFB
+                                        /////////////////////////CFB/////////////////////////
+                                        /////////////////////////CFB/////////////////////////
+                        InputStream inputCBC = server.getInputStream();
+
+
+
+                        Cipher cipherCBC = Cipher.getInstance("DES/CBC/NoPadding");
+                        cipherCBC.init(Cipher.DECRYPT_MODE, secretkey, ivspec);
+
+
+                        int valueCBC = 0;
+                        byte[] bufferCBC = new byte[8];
+                        byte[] bufferCBC2 = new byte[256];
+                        while((valueCBC=inputCBC.read(bufferCBC)) != -1) {
+                                for (i=0; i<8; i++)
+                                        bufferCBC2[i+length] = bufferCBC[i];
+                                length = length+8;
+                        }
+                        byte[] realdoCBC = new byte[length];
+                        for (i=0; i<length; i++)
+                                realdoCBC[i] = bufferCBC2[i];
+
+
+
+                        byte[] plainTextCBC = cipherCBC.doFinal(realdoCBC);
+                        System.out.print(new String(plainTextCBC));
+
+
+                        inputCBC.close();
+                        server.close();
+
+                        break;
+
+                case 4:                                 //CFB
+                                                        /////////////////////////CFB/////////////////////////
+                        InputStream inputOFB = server.getInputStream();
+
+
+
+                        Cipher cipherOFB = Cipher.getInstance("DES/OFB/NoPadding");
+                        cipherOFB.init(Cipher.DECRYPT_MODE, secretkey, ivspec);
+
+
+                        int valueOFB = 0;
+                        byte[] bufferOFB = new byte[8];
+                        byte[] bufferOFB2 = new byte[256];
+                        while((valueOFB=inputOFB.read(bufferOFB)) != -1) {
+                                for (i=0; i<8; i++)
+                                        bufferOFB2[i+length] = bufferOFB[i];
+                                length = length+8;
+                        }
+                        byte[] realdoOFB = new byte[length];
+                        for (i=0; i<length; i++)
+                                realdoOFB[i] = bufferOFB2[i];
+
+
+
+                        byte[] plainTextOFB = cipherOFB.doFinal(realdoOFB);
+                        System.out.print(new String(plainTextOFB));
+
+
+                        inputOFB.close();
+                        server.close();
+
+                        break;
+
+                case 5:                         //CFB
+                        /////////////////////////CFB/////////////////////////
+                        InputStream inputCTR = server.getInputStream();
+
+
+
+                        Cipher cipherCTR = Cipher.getInstance("DES/CTR/NoPadding");
+                        cipherCTR.init(Cipher.DECRYPT_MODE, secretkey, ivspec);
+
+
+                        int valueCTR = 0;
+                        byte[] bufferCTR = new byte[8];
+                        byte[] bufferCTR2 = new byte[256];
+                        while((valueCTR=inputCTR.read(bufferCTR)) != -1) {
+                                for (i=0; i<8; i++)
+                                        bufferCTR2[i+length] = bufferCTR[i];
+                                length = length+8;
+                        }
+                        byte[] realdoCTR = new byte[length];
+                        for (i=0; i<length; i++)
+                                realdoCTR[i] = bufferCTR2[i];
+
+
+
+                        byte[] plainTextCTR = cipherCTR.doFinal(realdoCTR);
+                        System.out.print(new String(plainTextCTR));
+
+
+                        inputCTR.close();
+                        server.close();
+
+                        break;
+
+                }
+        }
+}
 }
